e4d685b4c67d64f09db32f266310969f726af598
Kolodnikov Maxim <bredor419@gmail.com>
2018-07-14 16:24:20 +0700

    Gifts model added

--- project/gifts/models.py
+++ project/gifts/models.py
@@ -5,4 +5,4 @@ from users.models import User
 class Gift(models.Model):
     name = models.CharField(max_length=140)
     description = models.CharField(max_length=300, blank=True)
-    picture = models.ImageField()
+    picture = models.ImageField(upload_to='/uploads/%Y/%m/%d/')
---
+++ project/project/settings/media.py
@@ -0,0 +1,6 @@
+import os
+from .base import BASE_DIR
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'photogal', 'media')
+
+MEDIA_URL = '/media/'
