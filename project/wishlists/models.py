aceaa72af98a6e8176d23e59de8daae81e32a9f1
Kolodnikov Maxim <bredor419@gmail.com>
2018-07-14 16:49:53 +0700

    Squad app added

--- project/wishlists/models.py
+++ project/wishlists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
+from gifts.models import Gift
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    name = models.CharField(max_filed=140)
+    owner = models.ForeignKey(
+        User, related_name='wishlists', on_delete=models.CASCADE)
+    gifts = models.ManyToManyField(Gift, related_name='wishlists')
